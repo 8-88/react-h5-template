2eb63b138f7b35caa1354495d8def208d14cceae
8-88 <84121208+8-88@users.noreply.github.com>
2024-03-27 23:24:18 +0800

    Update .umirc.prod.js

--- .umirc.prod.js
+++ .umirc.prod.js
@@ -2,7 +2,7 @@ import { defineConfig } from "umi";
 
 export default defineConfig({
   define: {
-    baseApi: "https://api.somedomain.com",
+    baseApi: "https://8-88.github.io/react-h5-template",
     "process.env": {
       NODE_ENV: "uat",
       UMI_ENV: "uat",
